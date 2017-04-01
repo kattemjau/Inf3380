8a742c21742448f7cc3634a82098da4c316a5cd3
root <root@localhost.localdomain>
2017-04-01 21:21:54 +0200

    wadafak

--- Oblig1/serial_main.c
+++ Oblig1/serial_main.c
@@ -16,73 +16,99 @@ void export_JPEG_file(const char *filename, unsigned char *image_chars,
     int num_components, int quality);
 
 
-int main(int argc, char *argv[]){
-    int m, n, c, iters;
-    float kappa;
-    image u, u_bar;
-    unsigned char *image_chars;
-    char *input_jpeg_filename, *output_jpeg_filename;
-    /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
-    /* ... */
-    if(argc<5){
-      printf("Wrong args! Usage: ./filename <kappa> <iters> <inutfile> <outputfile>\n");
-    }
-    input_jpeg_filename=argv[3];
-    output_jpeg_filename=argv[4];
-    import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
-
-    allocate_image (&u, m, n);
-
-    allocate_image (&u_bar, m, n);
-
-    convert_jpeg_to_image (image_chars, &u);
-
-    iso_diffusion_denoising (&u, &u_bar, kappa, iters);
-
-    convert_image_to_jpeg (&u_bar, image_chars);
-
-    export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
-
-    deallocate_image (&u);
-    deallocate_image (&u_bar);
-    return 0;
-}
 
 void allocate_image(image *u, int m, int n){
   //m er horisontal n er vertikal
   u->n=n;
   u->m=m;
 
-  u->image_data=(float**)malloc(m*sizeof(float**));
-
-  for(int i=0; i<n; i++){
-      u->image_data[i]=malloc(sizeof(float*)*n);
+  u->image_data=(float**)malloc(m*sizeof(float*));
+   int i;
+  for(i=0; i<n; i++){
+      u->image_data[i]=(float*)malloc(sizeof(float)*n);
   }
 
 }
 void deallocate_image(image *u){
-  for(int i=0; i<n; i++){
+  int i;
+  for(i=0; i<u->m; i++){
       free(u->image_data[i]);
   }
   free(u->image_data);
 
 }
 void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
-  //make 1d array(struct) to 2d array
-
+  //convert 1d array to 2d array
+  int i, k;
+  for (i = 0; i < u->m; i++) {
+    for (k = 0; k < u->n; k++) {
+      u->image_data[i][k]=image_chars[i*u->n+k];
+    }
+  }
 }
-void convert_image_to_jpeg(const image *u, unsignes char* image_chars){
+void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
   //make 2d array(struct) to 1d array
-
-
+  int i, k;
+  for (i = 0; i < u->m; i++) {
+    for (k = 0; k < u->n; k++) {
+      image_chars[i*u->n+k]=u->image_data[i][k];
+    }
+  }
 }
-void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters){
+
+
+void iso_diffusion_denoising(const image* u, image *u_bar, float kappa, int iters){
   //smoothening function
+  int i, p, j, k;
+
+  for (p = 0; p < iters; p ++) {
+
+  for (i = 1; i < (*u).m - 1; i ++) {
+      for (j = 1; j < (*u).n - 1; j ++) {
+          u_bar->image_data[i][j] = u->image_data[i][j] + kappa * (u->image_data[i - 1][j] + u->image_data[i + 1][j]  + u->image_data[i][j - 1] + u->image_data[i][j + 1] - 4 * u->image_data[i][j]);
+
+      }
+  }
+  //copy new to old to start agein
+  for (i = 0; i < u->m; i++) {
+    for (k = 0; k < u->n; k++) {
+      u->image_data[i][k]=u_bar->image_data[i][k];
+    }
+  }
 }
-import_JPEG_file(char* input_jpeg_filename, int image_chars, int m,int n,int c){
-  //import picture and change input_jpeg_filename pointer
 }
 
-export_JPEG_file(char* output_jpeg_filename, int image_chars, int m, int n, int c, int tall){
-  //export picture to output_jpeg_filename
+
+
+int main(int argc, char *argv[]){
+  printf("starting\n");
+  int m, n, c, iters;
+  float kappa;
+  image u, u_bar;
+  unsigned char *image_chars;
+  char *input_jpeg_filename, *output_jpeg_filename;
+  /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
+  /* ... */
+  if(argc<5){
+    printf("Wrong args! Usage: ./filename <kappa> <iters> <inutfile> <outputfile>\n");
+  }
+  input_jpeg_filename=argv[3];
+  output_jpeg_filename=argv[4];
+  import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
+
+  allocate_image (&u, m, n);
+
+  allocate_image (&u_bar, m, n);
+
+  convert_jpeg_to_image (image_chars, &u);
+
+  iso_diffusion_denoising (&u, &u_bar, kappa, iters);
+
+  convert_image_to_jpeg (&u_bar, image_chars);
+
+  export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
+
+  deallocate_image (&u);
+  deallocate_image (&u_bar);
+  return 0;
 }
