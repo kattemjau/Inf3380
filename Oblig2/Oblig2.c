c83b89771664c5585a535b44e3cdf9ffa45f53b4
kattemjau <sindre.c.h@hotmail.com>
2017-05-16 18:00:11 +0200

    this

--- Oblig2/Oblig2.c
+++ Oblig2/Oblig2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+// #include <math.h>
 
 void compute(double** matrix1, double** matrix2, double* matrix3, int arow, int bcols, int brow){
 	double sum=0;
@@ -44,6 +45,16 @@ void read_matrix_binaryformat (char* filename, double*** matrix, int* num_rows, 
 			fwrite (matrix, sizeof(double), num_rows*num_cols, fp);
 			fclose (fp);
 		}
+		void skrivOmB(double** matrix2, double* matrixB, int rows, int cols){
+			int i, k;
+			for(i=0;i<rows;i++){
+				for(k=0; k<cols;k++){
+					matrixB[i*k+k]=matrix2[k][i];
+				}
+			}
+		}
+
+
 
 		int main(int argc, char *argv[]) {
 			char* file1;
@@ -53,41 +64,76 @@ void read_matrix_binaryformat (char* filename, double*** matrix, int* num_rows, 
 			double** matrix2;
 			double* matrix3;
 			int num_rows1, num_cols1, num_rows2, num_cols2, num_rows3, num_cols3;
-			int num_procs, my_rank;
-
-
-			if(argc!=4){
-				printf("Usage: ./'program' file1 file2 outputfile\n");
-			}
-			file1=argv[1];
-			file2=argv[2];
-			outputfile=argv[3];
-			printf("\nFile1: %s File 2: %s File3: %s\n",file1, file2, outputfile);
+			int num_procs, my_rank, sqr;
 
 			//start open mpi
 			MPI_Init (&argc, &argv);
-		  MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
-		  MPI_Comm_size (MPI_COMM_WORLD, &num_procs);
+			MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+			MPI_Comm_size (MPI_COMM_WORLD, &num_procs);
 
 			if(my_rank==0){
+
+				if(argc!=4){
+					printf("Usage: ./'program' file1 file2 outputfile\n");
+				}
+				file1=argv[1];
+				file2=argv[2];
+				outputfile=argv[3];
+				printf("\nFile1: %s File 2: %s File3: %s\n",file1, file2, outputfile);
+
 			//read matrix 1 and 2
 				read_matrix_binaryformat(file1, &matrix1, &num_rows1, &num_cols1);
 				read_matrix_binaryformat(file2, &matrix2, &num_rows2, &num_cols2);
-				//sets values
+
 				if(num_rows1>=num_rows2){	num_rows3=num_rows1;
 				}else{num_rows3=num_rows2;
 				}if(num_cols1>=num_cols2){num_cols3=num_cols1;
 				}else{num_cols3=num_cols2;}
 
-				printf("Allocate\n" );
-				printf("matrix 3: Rows: %d Cols: %d\n",num_rows3, num_cols3 );
-				matrix3 = (double*)malloc((num_rows3)*(num_cols3)*sizeof(double));
+				sqr=2;
+				// printf("sqr: %d\n",sqr );
+
 			}
 
-			MPI_Bcast (&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-			MPI_Bcast (&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			// MPI_Bcast (&sqr, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			// MPI_Bcast (&num_cols1, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			// MPI_Bcast (&num_rows2, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			// MPI_Bcast (&num_cols2, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 			//dele opp arbeids oppgaver
+			if(my_rank==0){
+				double* matrixb[num_rows2*num_cols2];
+				skrivOmB(matrix2, matrixb, num_rows2, num_cols2);
+				deallocate(matrix2);
+
+
+				int loddrett=num_cols3/sqr;
+				int pos=num_rows3*loddrett;
+				int restRow=num_rows3%loddrett;
+
+				int restCol=num_cols3%cols;
+				int i, rows;
+		    for(i=0; i<num_procs; i++){
+					if(i<restRow){
+						pos++;
+					}
+					rows=i/sqr; //hvilken firkant den er i
+					//send matrix 1
+					MPI_Send(matrix1[0][rows*pos], pos, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+					//send matrix 2
+					MPI_Send(matrixb[i], num_cols2, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+				}
+				deallocate(matrix1);
+
+			}else{
+				printf("My rank %d\n", my_rank );
+				//allocate matrix
+				matrix1=(double*)malloc((num_rows3)*(num_cols3)*sizeof(double));
+				matrix2=(double*)malloc((num_rows3)*(num_cols3)*sizeof(double));
+
+				// MPI_Recv();
+				// MPI_Recv();
+			}
 
 			printf("Compute\n");
 			// int arow, int bcols, int brow
@@ -95,15 +141,23 @@ void read_matrix_binaryformat (char* filename, double*** matrix, int* num_rows, 
 
 
 			if(my_rank==0){
+				//sets values
+
+
+				// printf("Allocate\n" );
+				printf("matrix 3: Rows: %d Cols: %d\n",num_rows3, num_cols3 );
+				matrix3 = (double*)malloc((num_rows3)*(num_cols3)*sizeof(double));
+				//recieve data from children
+
+
 				printf("Write to file\n");
 				//write matrix
 				write_matrix_binaryformat(outputfile, matrix3, num_rows3, num_cols3);
 
 				printf("deallocate\n" );
-				deallocate(matrix1);
-				deallocate(matrix2);
-				free(matrix3);				
+				free(matrix3);
 			}
 
+			MPI_Finalize();
 			return 0;
 		}
