1e5c61edba91ca744810b2c0046a975dd4349b72
kattemjau <sindre.c.h@hotmail.com>
2017-05-16 13:37:31 +0200

    sekvensiell fungerer, starter paa parralell

--- Oblig2/Oblig2.c
+++ Oblig2/Oblig2.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
-void compute(double** matrix1, double** matrix2, double** matrix3, int arow, int bcols, int acols){
-	int sum=0, k, d, c;
+void compute(double** matrix1, double** matrix2, double* matrix3, int arow, int bcols, int brow){
+	double sum=0;
+	int k, d, c;
 	for (c = 0; c < arow; c++) {
 		for (d = 0; d < bcols; d++){
-			for (k = 0; k < acols; k++) {
+			for (k = 0; k < brow; k++) {
 				//1d array ikke 2d
-				sum += (*matrix1[c]+k)*(*matrix2[k]+d);
+				sum += (matrix1[c][k])*(matrix2[k][d]);
 			}
-
-			matrix3[c][d] = sum;
-			printf("%x ",sum );
+			// printf("%d %d\n",c, d );
+			matrix3[(c*arow)+d] = sum;
 			sum = 0;
 		}
-		printf("\n");
+		// printf("\n");
 	}
 }
-void allocateMatrix(double*** matrix, int num_rows, int num_cols){
-	int i;
-	*matrix = (double**)malloc((num_rows)*sizeof(double*));
-	(*matrix)[0] = (double*)malloc((num_rows)*(num_cols)*sizeof(double));
-	for (i=1; i<(num_rows); i++)
-	(*matrix)[i] = (*matrix)[i-1]+(num_cols);
-}
 void deallocate(double** matrix){
-	int i;
 	free(*matrix);
 	free(matrix);
 }
@@ -44,11 +37,11 @@ void read_matrix_binaryformat (char* filename, double*** matrix, int* num_rows, 
 		fread ((*matrix)[0], sizeof(double), (*num_rows)*(*num_cols), fp);
 		fclose (fp);
 	}
-	void write_matrix_binaryformat (char* filename, double** matrix, int num_rows, int num_cols){
+	void write_matrix_binaryformat (char* filename, double* matrix, int num_rows, int num_cols){
 			FILE *fp = fopen (filename,"wb");
 			fwrite (&num_rows, sizeof(int), 1, fp);
 			fwrite (&num_cols, sizeof(int), 1, fp);
-			fwrite (matrix[0], sizeof(double), num_rows*num_cols, fp);
+			fwrite (matrix, sizeof(double), num_rows*num_cols, fp);
 			fclose (fp);
 		}
 
@@ -58,13 +51,9 @@ void read_matrix_binaryformat (char* filename, double*** matrix, int* num_rows, 
 			char* outputfile;
 			double** matrix1;
 			double** matrix2;
-			double** matrix3;
-			int num_rows1;
-			int num_cols1;
-			int num_rows2;
-			int num_cols2;
-			int num_rows3;
-			int num_cols3;
+			double* matrix3;
+			int num_rows1, num_cols1, num_rows2, num_cols2, num_rows3, num_cols3;
+			int num_procs, my_rank;
 
 
 			if(argc!=4){
@@ -76,31 +65,45 @@ void read_matrix_binaryformat (char* filename, double*** matrix, int* num_rows, 
 			printf("\nFile1: %s File 2: %s File3: %s\n",file1, file2, outputfile);
 
 			//start open mpi
+			MPI_Init (&argc, &argv);
+		  MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+		  MPI_Comm_size (MPI_COMM_WORLD, &num_procs);
 
+			if(my_rank==0){
 			//read matrix 1 and 2
-			read_matrix_binaryformat(file1, &matrix1, &num_rows1, &num_cols1);
-			read_matrix_binaryformat(file2, &matrix2, &num_rows2, &num_cols2);
-			//sets values
-			if(num_rows1>=num_rows2){	num_rows3=num_rows1;
-			}else{num_rows3=num_rows2;
-			}if(num_cols1>=num_rows2){num_cols3=num_cols1;
-			}else{num_cols3=num_cols2;}
+				read_matrix_binaryformat(file1, &matrix1, &num_rows1, &num_cols1);
+				read_matrix_binaryformat(file2, &matrix2, &num_rows2, &num_cols2);
+				//sets values
+				if(num_rows1>=num_rows2){	num_rows3=num_rows1;
+				}else{num_rows3=num_rows2;
+				}if(num_cols1>=num_cols2){num_cols3=num_cols1;
+				}else{num_cols3=num_cols2;}
 
-			printf("Allocate\n" );
-			allocateMatrix(&matrix3, num_rows3, num_cols3);
+				printf("Allocate\n" );
+				printf("matrix 3: Rows: %d Cols: %d\n",num_rows3, num_cols3 );
+				matrix3 = (double*)malloc((num_rows3)*(num_cols3)*sizeof(double));
+			}
+
+			MPI_Bcast (&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			MPI_Bcast (&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+			//dele opp arbeids oppgaver
 
 			printf("Compute\n");
-			// int arow, int bcols, int acols
-			compute(matrix1, matrix2, matrix3, num_rows1, num_cols2, num_cols1);
+			// int arow, int bcols, int brow
+			compute(matrix1, matrix2, matrix3, num_rows1, num_cols2, num_rows2);
 
-			printf("Write to file\n");
-			//write matrix
-			write_matrix_binaryformat(outputfile, matrix3, num_rows3, num_cols3);
 
-			printf("deallocate\n" );
-			// deallocate(matrix1);
-			// deallocate(matrix2);
-			// deallocate(matrix3);
+			if(my_rank==0){
+				printf("Write to file\n");
+				//write matrix
+				write_matrix_binaryformat(outputfile, matrix3, num_rows3, num_cols3);
+
+				printf("deallocate\n" );
+				deallocate(matrix1);
+				deallocate(matrix2);
+				free(matrix3);				
+			}
 
 			return 0;
 		}
